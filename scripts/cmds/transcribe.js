752c5e646c633da7a57504afbb50c00613b55dc9
MehidiALPHONSE <137520052+MehidiALPHONSE@users.noreply.github.com>
2024-03-29 03:00:00 +0600

    Update transcribe.js

--- scripts/cmds/transcribe.js
+++ scripts/cmds/transcribe.js
@@ -1,47 +1,29 @@
-const axios = require("axios");
-
 module.exports = {
   config: {
-    name: "transcribe",
-    version: "1.1",
-    author: "mahim",
-    countDown: 10,
+    name: "imagine",
+    aliases: [gen],
+    author: "mahim|samir api",
+    version: "1.0",
+    cooldowns: 5,
     role: 0,
     shortDescription: {
-      vi: "Chuyển đổi lời nói thành văn bản.",
-      en: "Converts speech into text."
+      en: "Generate an image based on user input."
     },
     longDescription: {
-      vi: "Lệnh `transcribe` cho phép bạn trích xuất văn bản từ video hoặc âm thanh. Chỉ cần trả lời âm thanh hoặc video và lệnh sẽ sử dụng API để trích xuất văn bản từ âm thanh hoặc video. Văn bản trích xuất sẽ được gửi lại dưới dạng trả lời tin nhắn của bạn.",
-      en: "The `transcribe` command allows you to extract texts from videos or audios. Simply reply to an audio or video, and the command will use the API to extract the text from the audio or video. The extracted text will be sent back as a reply to your message."
+      en: "Generate an image based on user input and task ID."
     },
-    category: "tools",
+    category: "media",
     guide: {
-      vi: "{pn} trả lời âm thanh/video",
-      en: "{pn} reply to an audio/video"
+      en: "{prefix}imagine {prompt} | {model}"
     }
   },
+  onStart: async function({ message, args }) {
+    const p = args.join(" ");
 
-  onStart: async function({ event, api, message }) {
     try {
-      if (!event.messageReply.attachments || event.messageReply.attachments.length === 0) {
-        return message.reply('Please reply to an audio or video.');
-      }
-
-      const link = event.messageReply.attachments[0].url;
-      const response = await axios.get(`https://milanbhandari.imageapi.repl.co/transcribe?url=${encodeURIComponent(link)}`);
-      const text = response.data.transcript;
-
-      if (text) {
-        message.reply({
-          body: text
-        });
-      } else {
-        message.reply("Failed to transcribe the audio or video.");
-      }
-    } catch (error) {
-      console.error(error);
-      message.reply("An error occurred while processing the request.");
+      message.reply({ attachment: await global.utils.getStreamFromUrl(`https://apis-samir.onrender.com/api/generateImage?style=16&prompt=${p}&aspectRatio=16%3A9`) });
+    } catch (e) {
+      console.error(e); // by potato allou alu 
     }
   }
 };
